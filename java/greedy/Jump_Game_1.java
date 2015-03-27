4c00596e3be7683a59cf804db25d437760f56302
KendaG <thunderlbc@gmail.com>
2015-03-27 20:54:00 +0800

    Update Jump_Game_1.java

--- java/greedy/Jump_Game_1.java
+++ java/greedy/Jump_Game_1.java
@@ -1,51 +1,24 @@
 public class Solution {
-
     public boolean canJump(int[] A) {
-
        if(A.length ==1)
-
             return A[0] >= 0;
-
         int Pilot = 0;
-
         int max = 0;
-
         int rePilot = -1;
-
         int temp = 0;
-
         while (rePilot != Pilot) {
-
-
-
             rePilot = Pilot;
-
             for(int j = Pilot + 1; j < A[Pilot] + Pilot + 1; j++){
-
                 int se =A[j] + j ;
-
                 if(se > max){
-
                     max = se;
-
                     if(se >= A.length - 1)
-
                         return true;
-
                     temp = j;
-
                 }
-
             }
-
             Pilot = temp;
-
-
-
         }
-
         return false;
-
     }
-
 }
